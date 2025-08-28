154eea5dc2ec6265829c1170a5313e7dd8b58f36
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2025-08-28 12:37:26 -0500

    Apply .gitignore correctly

---
+++ StarWarsCombine/StarWarsCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsCombineApp.swift
+//  StarWarsCombine
+//
+//  Created by Juan Francisco Marcenaro Arellano on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
